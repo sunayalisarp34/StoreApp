bb48b4e550d32eacf360aa87c1dd3de6ac448310
sunayalisarp34 <129750287+sunayalisarp34@users.noreply.github.com>
2025-03-01 15:07:13 +0300

    finished working with services for now

---
+++ Services/Contracts/IServiceManager.cs
@@ -0,0 +1,7 @@
+﻿namespace Services.Contracts;
+
+public interface IServiceManager
+{
+    IProductService ProductService { get; }
+    ICategoryService CategoryService { get; }
+}
--- StoreApp/Controllers/ProductController.cs
+++ StoreApp/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
 using Repositories.Contracts;
+using Services.Contracts;
 using System.Runtime.CompilerServices;
 
 namespace StoreApp.Controllers
@@ -13,22 +14,22 @@ namespace StoreApp.Controllers
         //{
         //    return View();
         //}
-        private readonly IRepositoryManager _manager;
-        public ProductController(IRepositoryManager manager)
+        private readonly IServiceManager _manager;
+        public ProductController(IServiceManager manager)
         {
             _manager = manager;
         }
 
         public IActionResult Index() 
         {
-            var model = _manager.Product.GetAllProducts(false);
+            var model = _manager.ProductService.GetAllProducts(false);
 
             return View(model);
         }
-        public IActionResult Get(int id) 
+        public IActionResult Get([FromRoute(Name ="id")] int id) 
         {
             //Product product = _context.Products.First(p => p.ProductId.Equals(id));
-            var model = _manager.Product.GetOneProduct(id, false);
+            var model = _manager.ProductService.GetOneProduct(id, false);
             return View(model);
         }
 
